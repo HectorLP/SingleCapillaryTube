6ea958d6ccd5f2a080bd3af21d685193402de011
Hector.Lee <lipeihydrogeology@gmail.com>
2015-07-07 19:45:42 +0300

    Start the properties of tube and fluids.

---
+++ header/DynamicSingleTube.h
@@ -0,0 +1,18 @@
+#ifndef DYNAMICSINGLETUBE_H_
+#define DYNAMICSINGLETUBE_H_
+
+struct TubeGeometry
+{
+	double radius;
+	double length;
+	double angleToHorizontal
+};
+
+struct FluidProperties
+{
+	double densityWetting;
+	double densityNonWetting;
+	double dynamicViscosity;
+	double 
+};
+#endif 
